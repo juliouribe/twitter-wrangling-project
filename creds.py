4ce94d688142691c07e4ac63080fb0286b7db776
Julio Uribe <uribejulioc@gmail.com>
2019-04-01 17:01:20 -0700

    created credentials file to ignore

---
+++ creds.py
@@ -0,0 +1,4 @@
+consumer_key = 'K0aisHoHRVYI7h64IxrPMOXr0'
+consumer_secret = 'pu4ahP5QKQWZ4i56RbnZEdhBFlNPFpA0B4TiOHtwdihv6GuRsd'
+access_token = '4807406474-LXAmApzVQad4voKqG1XmpRlq1jQp8N4CIIzYnP8'
+access_secret = 'SLdSWJJ56Ppk15ApiCaeU0wMPWweHzLCbdfsoOud2SQ1z'
